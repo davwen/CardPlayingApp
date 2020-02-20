e221f10ee9301304855f6ffabc5e42032575b161
davwen <56315951+davwen@users.noreply.github.com>
2020-02-20 20:48:17 +0100

    Removed unnecessary code and TODO comments inside the fragments.

--- app/src/main/java/com/wenzel/cardplayingapp/PossibilitiesFragment.kt
+++ app/src/main/java/com/wenzel/cardplayingapp/PossibilitiesFragment.kt
@@ -22,35 +22,9 @@ import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.add_card_dialog.view.*
 import kotlinx.android.synthetic.main.fragment_possibilities.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [PossibilitiesFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [PossibilitiesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PossibilitiesFragment : Fragment() {
     private val addedCards = mutableListOf<Card>()
-
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private var listener: OnFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -164,59 +138,6 @@ class PossibilitiesFragment : Fragment() {
         return view
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listener?.onFragmentInteraction(uri)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PossibilitiesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PossibilitiesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
     private fun showAddDialog(context: Context, activity: Activity, adapter: Add_card_adapter){
 
         val builder = AlertDialog.Builder(context)
@@ -242,10 +163,5 @@ class PossibilitiesFragment : Fragment() {
 
     private fun onAddReturn(_card : Card, adapter: Add_card_adapter){
         adapter.addCard(_card)
-    }
-
-    tailrec fun Context.activity(): Activity? = when {
-        this is Activity -> this
-        else -> (this as? ContextWrapper)?.baseContext?.activity()
     }
 }
